6bdd0ac736b5051aae479b2e8d1295a8ffe97b1d
Catailog <tlsdudghks11@gmail.com>
2026-02-02 14:49:20 +0900

    fix: 캐싱을 하지 않던 문제 수정

--- src/api.js
+++ src/api.js
@@ -1,5 +1,12 @@
 export async function getPokemon(id) {
   const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
-  const res = await fetch(`${BASE_URL}/${id}`);
+  const URL = `${BASE_URL}/${id}`;
+
+  const res = await fetch(URL);
+
+  if (!res.ok) {
+    throw new Error(`네트워크 응답 에러: ${res.status}`);
+  }
+
   return await res.json();
 }
